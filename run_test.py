1966190d7a6a9b5e90a8585493db04fe1150ebe3
Aralas <14300180119@fudan.edu.cn>
2019-07-11 14:49:01 +0800

    experiment result

--- run_test.py
+++ run_test.py
@@ -32,18 +32,18 @@ seed = 99
 noise_level = 0.8
 clean_data_size = 200
 additional_data_limitation = [100, 2000]
-positive_threshold = 0.95
+positive_threshold = 0.9
 add_criterion = 95
 K_iteration = 10
 N_bagging = 100
-label = 1
+label = 0
 
 batch_size = 32
 epochs = 20
-learning_rate = 0.0001
+learning_rate = 0.00003
 data_augmentation = True
 
-path_name = '/positive_threshold_%.2f_add_criterion_%d_learning_rate_%.4f'%(positive_threshold, add_criterion, learning_rate)
+path_name = '/positive_threshold_%.2f_add_criterion_%d_learning_rate_%.5f'%(positive_threshold, add_criterion, learning_rate)
 model_dir = os.path.join(os.getcwd(), 'saved_models')
 if label == None:
     model_dir += path_name
@@ -55,7 +55,7 @@ precision_dir += path_name
 print(model_dir)
 print(precision_dir)
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
