081405f2aa9909950ef09ac8a35831393584a192
Aralas <14300180119@fudan.edu.cn>
2019-07-17 13:12:18 +0800

    fix error

--- run_benchmark_ResNet_pre-activation.py
+++ run_benchmark_ResNet_pre-activation.py
@@ -12,7 +12,22 @@ noise_level: fraction of noisy labels for the rest data
 data_augmentation: whether to use data augmentation in training
 n: number of blocks in ResNet
 
+# Model parameter
+# ----------------------------------------------------------------------------
+#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
+# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
+#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
+# ----------------------------------------------------------------------------
+# ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
+# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
+# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
+# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
+# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
+# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
+# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
+# ---------------------------------------------------------------------------
 """
+
 from __future__ import print_function
 import os
 from copy import deepcopy
@@ -20,17 +35,22 @@ import keras
 import numpy as np
 import tensorflow as tf
 from keras import backend as K
-from keras.datasets import cifar10
-from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras.callbacks import ReduceLROnPlateau
-from generate_ResNet import ResNet
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import AveragePooling2D, Input, Flatten
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.regularizers import l2
+
 
 '''
 ***** Set parameters *****
 '''
 seed = 99
-noise_level = 0.8
+noise_level = 0
 clean_data_size = 200
 
 batch_size = 64
@@ -42,7 +62,7 @@ n = 2
 depth = n * 9 + 2
 file_index = 0
 
-path_name = '/benchmark_ResNet%d_pre-activation_lr_%.4f' % (depth, learning_rate)
+path_name = '/benchmark_ResNet%d_pre-activation_lr_%.4f_noise_%.1f' % (depth, learning_rate, noise_level)
 model_dir = os.path.join(os.getcwd(), 'saved_models')
 model_dir += path_name
 precision_dir = os.path.join(os.getcwd(), 'saved_precision')
@@ -121,9 +141,179 @@ y_train[noise_index] = tf.contrib.keras.utils.to_categorical(new_label, 10)
 x_train = np.concatenate((x_train, x_clean), axis=0)
 y_train = np.concatenate((y_train, y_clean), axis=0)
 
-# Generate model.
-model_object = ResNet(10, input_shape, learning_rate, n)
 
+def lr_schedule(epoch):
+    """Learning Rate Schedule
+
+    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+    Called automatically every epoch as part of callbacks during training.
+
+    # Arguments
+        epoch (int): The number of epochs
+
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 1e-5
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+
+
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                   activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+    """2D Convolution-Batch Normalization-Activation stack builder
+
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
+
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
+
+def resnet_v2(input_shape, depth, num_classes=10):
+    """ResNet Version 2 Model builder [b]
+
+    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
+    bottleneck layer
+    First shortcut connection per layer is 1 x 1 Conv2D.
+    Second and onwards shortcut connection is identity.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filter maps is
+    doubled. Within each stage, the layers have the same number filters and the
+    same filter map sizes.
+    Features maps sizes:
+    conv1  : 32x32,  16
+    stage 0: 32x32,  64
+    stage 1: 16x16, 128
+    stage 2:  8x8,  256
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+    # Start model definition.
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+
+    inputs = Input(shape=input_shape)
+    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
+    x = resnet_layer(inputs=inputs,
+                     num_filters=num_filters_in,
+                     conv_first=True)
+
+    # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2    # downsample
+
+            # bottleneck residual unit
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters_in,
+                             kernel_size=1,
+                             strides=strides,
+                             activation=activation,
+                             batch_normalization=batch_normalization,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_in,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_out,
+                             kernel_size=1,
+                             conv_first=False)
+            if res_block == 0:
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters_out,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = keras.layers.add([x, y])
+
+        num_filters_in = num_filters_out
+
+    # Add classifier on top.
+    # v2 has BN-ReLU before Pooling
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr=lr_schedule(0)),
+                  metrics=['accuracy'])
+    return model
+
+
+"""
+# Generate model.
+"""
+model = resnet_v2(input_shape, depth, num_classes)
 model_name = 'cifar10_file%d.h5' % file_index
 filepath = os.path.join(model_dir, model_name)
 
@@ -133,7 +323,7 @@ checkpoint = ModelCheckpoint(filepath=filepath,
                              verbose=1,
                              save_best_only=True)
 
-lr_scheduler = LearningRateScheduler(model_object.lr_schedule)
+lr_scheduler = LearningRateScheduler(lr_schedule)
 
 lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
                                cooldown=0,
@@ -145,12 +335,12 @@ callbacks = [checkpoint, lr_reducer, lr_scheduler]
 # Run training, with or without data augmentation.
 if not data_augmentation:
     print('Not using data augmentation.')
-    History = model_object.model.fit(x_train, y_train,
-                                      batch_size=batch_size,
-                                      epochs=epochs,
-                                      validation_data=(x_test, y_test),
-                                      shuffle=True,
-                                      callbacks=callbacks)
+    History = model.fit(x_train, y_train,
+                      batch_size=batch_size,
+                      epochs=epochs,
+                      validation_data=(x_test, y_test),
+                      shuffle=True,
+                      callbacks=callbacks)
 else:
     print('Using real-time data augmentation.')
     # This will do preprocessing and realtime data augmentation:
@@ -201,15 +391,15 @@ else:
     datagen.fit(x_train)
 
     # Fit the model on the batches generated by datagen.flow().
-    History = model_object.model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
-                                                steps_per_epoch=x_train.shape[0],
-                                                validation_data=(x_validation, y_validation),
-                                                epochs=epochs, verbose=1, workers=4,
-                                                callbacks=callbacks)
+    History = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                                    steps_per_epoch=x_train.shape[0],
+                                    validation_data=(x_validation, y_validation),
+                                    epochs=epochs, verbose=1, workers=4,
+                                    callbacks=callbacks)
 
 # Score trained model.
-model_object.model.load_weights(filepath)
-scores = model_object.model.evaluate(x_test, y_test, verbose=1)
+model.load_weights(filepath)
+scores = model.evaluate(x_test, y_test, verbose=1)
 
 # Record accuracy.
 record_file = open(os.path.join(precision_dir, 'accuracy_file%d.txt' % file_index), 'a+')
@@ -217,17 +407,4 @@ record_file.write('training accuracy\n' + str(History.history['acc']))
 record_file.write('\nvalidation accuracy\n' + str(History.history['val_acc']))
 record_file.write('\ntest accuracy\n' + str(scores[1]))
 record_file.close()
-
-
-
-
-
-
-'''
-## Train teacher model on additional clean data
-'''
-
-'''
-## Train student model
-'''
 
